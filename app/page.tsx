664eee67654e2deee505bde767f1ff4988d9ec8b
Zohaib <zohaibwb38@gmail.com>
2024-09-09 23:23:59 +0500

    Random index error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import QuoteIcon from "@/assets/quote.svg";
 import Image from "next/image";
 import MentalHealthQuotes from "@/data/mental-health-quotes.json";
+
 interface Quote {
   id: number;
   quote: string;
   author: string;
 }
 
-const Home = async () => {
-  // let quote: Quote | null = null;
-  // try {
-  //   const res = await fetch("https://dummyjson.com/quotes/random");
-  //   quote = (await res.json()) as Quote;
-  // } catch (err) {
-  //   console.log(err);
-  // }
-  const quote: Quote =
-    MentalHealthQuotes[
-      Math.ceil(Math.random() * (MentalHealthQuotes.length - 1 - 0) + 0)
-    ];
+const Home = () => {
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  useEffect(() => {
+    // Generate a random index and fetch a quote when the component mounts
+    const randomIndex = Math.floor(Math.random() * MentalHealthQuotes.length);
+    setQuote(MentalHealthQuotes[randomIndex]);
+  }, []);
 
   return (
     <div className="flex justify-center items-center flex-col p-10 min-h-screen gap-10 bg-slate-900">
-      {!quote ? (
-        <h1 className="text-[30px]">Something went wrong, please try again</h1>
-      ) : (
+      {!quote ? null : (
         <>
           <Image src={QuoteIcon} alt="Quote Icon" width={70} />
           <blockquote
             cite="https://dummyjson.com/quotes/random"
             className="text-[30px] text-center leading-tight normal-case max-w-[800px]"
           >
-            {quote?.quote}
+            {quote.quote}
           </blockquote>
           <h4 className="text-lg">-{quote.author}</h4>
         </>
@@ -41,5 +39,3 @@ const Home = async () => {
 };
 
 export default Home;
-
-export const fetchCache = "force-no-store";
